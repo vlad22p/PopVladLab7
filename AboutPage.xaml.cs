33c2c582d3accf2e871330e592ef6356741c6e17
Vlad Pop <vlad.pop0797@gmail.com>
2024-11-19 17:46:30 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PopVladLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PopVladLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
