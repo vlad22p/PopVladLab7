1900056ddc3ee3532dd08ef3965c637af7e62a55
Vlad Pop <vlad.pop0797@gmail.com>
2024-11-26 18:18:18 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace PopVladLab7
+﻿using System;
+using PopVladLab7.Data;
+using System.IO;
+
+
+namespace PopVladLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopVladLab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+}
